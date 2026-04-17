bf6efd4e642fae397a3c5526f1e722f0af0bb32c
muruganramya2008-source <muruganramya2008@gmail.com>
2026-04-17 12:07:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -246,9 +246,9 @@ elif st.session_state.page == "Result":
     result = st.session_state.prediction_result
 
     if result == 1:
-        result_message = f"{patient_name} Yes:Lung Cancer."
+        result_message = f"{patient_name} : Yes Lung Cancer."
     else:
-        result_message = f"{patient_name} No:Lung Cancer."
+        result_message = f"{patient_name} : No Lung Cancer."
 
     st.markdown(
         f"""
